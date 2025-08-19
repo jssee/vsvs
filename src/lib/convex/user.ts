7ae9ff9a6557186250377d54ac4d8278ecd99e64
Jesse Hoyos <jesse@mojotech.com>
2025-08-18 23:17:21 -0400

    Initialize sveltekit + convex app

---
+++ src/lib/convex/user.ts
@@ -0,0 +1,9 @@
+import { query } from './_generated/server';
+
+export const get = query({
+	args: {},
+	handler: async (ctx) => {
+		const user = await ctx.db.query('user').collect();
+		return user;
+	}
+});
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { useQuery } from 'convex-svelte';
+	import { api } from '$lib/convex/_generated/api.js';
+
+	const query = useQuery(api.user.get, {});
+</script>
+
+{#if query.isLoading}
+	Loading...
+{:else if query.error}
+	failed to load: {query.error.toString()}
+{:else}
+	<h1>Hello {query.data[0].displayName}!</h1>
+{/if}
